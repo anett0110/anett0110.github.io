c86f1d3bac2e7becf11537ddc79e1ca0bf26d672
Daniel Kovacs <daniel.kovacs@inepex.com>
2023-03-30 22:32:09 +0200

    removed contact form

--- contact.php
+++ contact.php
@@ -174,7 +174,7 @@
 <!--                                    <h3 class="title text-upper" style="text-align: center">Az alábbi űrlapon keresztül-->
 <!--                                        is felveheted velem a kapcsolatot</h3>-->
                                 </header>
-                                <div class="mapouter"><div class="gmap_canvas"><iframe width="770" height="510" id="gmap_canvas" src="https://maps.google.com/maps?q=Budapest&t=&z=10&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://2yu.co">2yu</a><br><style>.mapouter{position:relative;text-align:right;height:510px;width:770px;}</style><a href="https://embedgooglemap.2yu.co">html embed google map</a><style>.gmap_canvas {overflow:hidden;background:none!important;height:510px;width:770px;}</style></div></div>
+                                <div class="mapouter"><div class="gmap_canvas"><iframe width="770" height="510" id="gmap_canvas" src="https://maps.google.com/maps?q=Budapest&t=&z=10&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><style>.mapouter{position:relative;text-align:center;height:510px;width:770px;}</style><style>.gmap_canvas {overflow:hidden;background:none!important;height:510px;width:770px;}</style></div></div>
                             </div>
 <!--                            <div class="content">-->
 <!--                                <div class="container">-->
